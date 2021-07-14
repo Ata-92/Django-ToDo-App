849992efaef7523007fdfecbf2c588f3c2e8021d
Ata-92 <atakal92@gmail.com>
2021-07-14 18:54:13 +0300

    Add delete url

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, todo_create, todo_list, todo_update
+from .views import home, todo_create, todo_delete, todo_list, todo_update
 
 urlpatterns = [
     path("", home, name="home_page"),
     path("list/", todo_list, name="todo-list"),
     path("create/", todo_create, name="create"),
     path("<int:id>/update/", todo_update, name="update"),
+    path("<int:id>/delete/", todo_delete, name="delete"),
 ]
