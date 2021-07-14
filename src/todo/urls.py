3678d1841e210016218295403b932640e96bbdf5
Ata-92 <atakal92@gmail.com>
2021-07-14 18:29:09 +0300

    Add update url

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, todo_create, todo_list
+from .views import home, todo_create, todo_list, todo_update
 
 urlpatterns = [
     path("", home, name="home_page"),
     path("list/", todo_list, name="todo-list"),
     path("create/", todo_create, name="create"),
+    path("<int:id>/update/", todo_update, name="update"),
 ]
